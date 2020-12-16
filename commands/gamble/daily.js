b10f2f828d5c15cb28de80edb90967cee1f1cff8
Lunnar <hoanghcm188@gmail.com>
2020-12-16 21:03:53 +0700

    update

--- commands/gamble/daily.js
+++ commands/gamble/daily.js
@@ -12,20 +12,8 @@ module.exports = {
     usage: 'daily',
     note: 'Quà hằng ngày!',
     run: async (client, message, args) => {
-        //let res = await instance.get(`/getdata?id=${message.author.id}`);
-        //res = res.data;
-        //if (res.data == false) {
-        //    const embed = new MessageEmbed()
-        //        .setTitle('Nhận tiền thất bại!')
-        //       .setDescription(`Bạn chưa upvote cho bot, bạn upvote bằng cách [click vào đây](https://top.gg/bot/645883401500622848/vote)`);
-        //    return message.channel.send(embed);
-        //} else {
-            const random = Math.floor(Math.random() * 50000);
+            const random = Math.floor(Math.random() * 5000);
             await eco.addMoney(message.author.id, random);
             message.channel.send(`:moneybag: Bạn vừa được cộng ${random} VNĐ!`);
-            await instance.post('/addmoney', {
-                id: message.author.id,
-            });
-        //}
     },
 };
